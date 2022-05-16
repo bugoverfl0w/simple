c1814ce76c009604c56e0afb4db2f38a7711f827
manutd <dev@localhost>
2022-05-16 16:43:04 +0700

    update

--- tmux-and-vim.sh
+++ tmux-and-vim.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y vim tmux git -y 2>/dev/null
+sudo apt-get install -y vim tmux git rsync -y 2>/dev/null
 curl -4 https://raw.githubusercontent.com/quydox/simple/master/.tmux.conf 2>/dev/null > ~/.tmux.conf
 curl -4 https://raw.githubusercontent.com/quydox/simple/master/vim.tar.gz 2>/dev/null > vim.tar.gz
 
