0d9633bc20fc9252376b52201090e801601e6da6
manutd <dev@localhost>
2022-05-16 08:47:24 +0700

    update

---
+++ tmux-and-vim.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/quydox/simple/master/.tmux.conf -O ~/.tmux.conf
+wget https://raw.githubusercontent.com/quydox/simple/master/vim.tar.gz
+
+tar xzf vim.tar.gz && mv vim/* ~/
+
+rm -rf vim
+
