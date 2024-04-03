5ef5be4360b38d9a115b5c49266d100a6dc35a19
bugoverflow <46780804+bugoverfl0w@users.noreply.github.com>
2024-04-03 15:38:46 +0700

    Update tmux-and-vim.sh

--- tmux-and-vim.sh
+++ tmux-and-vim.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y vim tmux git rsync -y 2>/dev/null
-curl -4 https://raw.githubusercontent.com/quydox/simple/master/.tmux.conf 2>/dev/null > ~/.tmux.conf
-curl -4 https://raw.githubusercontent.com/quydox/simple/master/vim.tar.gz 2>/dev/null > vim.tar.gz
+curl -4 https://raw.githubusercontent.com/bugoverfl0w/simple/master/.tmux.conf 2>/dev/null > ~/.tmux.conf
+curl -4 https://raw.githubusercontent.com/bugoverfl0w/simple/master/vim.tar.gz 2>/dev/null > vim.tar.gz
 
 tar xzf vim.tar.gz && rsync -avzhP vim/ ~/
 
